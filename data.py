e5d3d01bdd6191025009f201fc1aa44222caa35a
BlueM0ld <blu3m0ld@gmail.com>
2024-10-21 16:30:36 +0100

    update data.py

--- data.py
+++ data.py
@@ -12,12 +12,68 @@ df_train = pd.DataFrame(ds['train'])
 
 train_answers = df_train[['query', 'passages']][:11]
 
-num_of_rows = len(train_answers.index) - 1 
-#print(type(train_answers['passages']))
+num_of_rows = len(train_answers.index) - 1
+# print(type(train_answers['passages']))
 
 # list_of_tuples = [] # [(query_text, relevant_text, irrelevant_text)]
 # for passage in train_answers['passages']['passage_text']:
 #     print(passage)
+
+# TODO Data cleaning
+
+
+def clean_text(text):
+    text = text.lower()
+    text = text.replace('\n', ' ')
+    text = text.replace('\r', ' ')
+    text = text.replace('\t', ' ')
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace(' - ', ' <HYPHEN> ')
+    text = text.replace(' – ', ' <HYPHEN2> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace(':', ' <COLON> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace("'", ' <SINGLE QUOTATION_MARK> ')
+    # text = ascii(text)
+    words = text.split(" ")
+
+    return words
+
+# def add_words_to_dict(words, word_to_int):
+#     for word in words:
+#         word_to_int.get(word, counter)
+
+
+def clean_data(df):
+    print("cleaning data ...")
+    text_list = []
+    for index, row in df.iterrows():
+        text_list = text_list + clean_text(row['query'])
+        passages = row['passages']
+        text = ''
+        for passage in passages['passage_text']:
+            # text += passage.strip()
+            text_list = text_list + clean_text(passage)
+            # print(clean_text(passage))
+            # print(passage)
+
+    return text_list
+
+
+# probably need to call this on other columns too
+text_words = clean_data(train_answers)
+print(text_words)
+# passages = train_answers['passages']
+
+# clean_text(passages[0]['passage_text'][0])
+
 
 def get_random_text(index):
     random_index = random.randint(0, num_of_rows)
@@ -31,7 +87,8 @@ def get_random_text(index):
     random_passage_index = random.randint(0, len(irrelevant_passages)-1)
     random_passage = irrelevant_passages[random_passage_index]
     return random_passage
-    
+
+
 list_of_tuples = []
 
 for index, row in train_answers.iterrows():
@@ -40,19 +97,17 @@ for index, row in train_answers.iterrows():
 
     is_selected = passages['is_selected']
     passage_texts = passages['passage_text']
-    
+
     irrelevant_passages = []
     for i in range(10):
         irrelevant_passages.append(get_random_text(index))
-    
-    #print(random_index)
+
+    # print(random_index)
     # irrelevant_row = train_answers.iloc[random_index]
     # irrelevant_passage = irrelevant_row['passages']
     # irrelevant_passages = irrelevant_passage['passage_text']
     print(len(irrelevant_passages))
     list_of_tuples.append((query, passage_texts, irrelevant_passages))
-
-    
 
     # print(f"Row {index}:")
     # for i, passage in enumerate(passage_texts):
@@ -68,12 +123,11 @@ print(len(list_of_tuples[0][1]))
 print("irrelevant passages")
 print(len(list_of_tuples[0][2]))
 
-# df_test = pd.DataFrame(ds['test'])
-# print(df_test.head())
 
-# df_validation = pd.DataFrame(ds['validation'])
-# print(df_validation.head())
+# TODO Build a tokenizer
 
-# TODO negative sampling
-# query, valid document, invalid documents
-# take in a query --> eventually randomise the negative documents
+def tokenizer():
+    # take in a dataframe
+    # go row by row
+    # text replace ?
+    print("todo")
